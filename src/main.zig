299fcb79c386759e1cbe716139ba281f5d7a799b
Ivan Stamenkovic <ivan@seemsindie.com>
2026-04-09 18:23:22 +0200

    Bump version to 0.3.1-beta.2

--- src/main.zig
+++ src/main.zig
@@ -10,7 +10,7 @@ const deps_cmd = @import("commands/deps.zig");
 const update_cmd = @import("commands/update.zig");
 const assets_cmd = @import("commands/assets.zig");
 
-const version = "0.3.1-beta.1";
+const version = "0.3.1-beta.2";
 
 pub fn main(init: std.process.Init) !void {
     const allocator = init.gpa;
